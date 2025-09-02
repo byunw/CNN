c9549515f6ae495c7ae7d9b308fffe77e8061d85
wb4248 <wb4248@rit.edu>
2025-09-02 11:37:02 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import torchvision
+from torchvision.transforms import ToTensor
+import torch.nn as nn
+import torch.nn.functional as F
+
+train_dataset = torchvision.datasets.CIFAR10(root='data/',train=True,transform=ToTensor())
+test_dataset = torchvision.datasets.CIFAR10(root='data/',train=False,transform=ToTensor())
+
+#test code
+assert train_dataset.data.size == 153600000
+assert test_dataset.data.size == 30720000
+
+assert train_dataset.data[0][:,:,0].size == 1024 #train_dataset.data[0][:,:,0] has pixels of the first layer of the first image
+assert train_dataset.data[0][:,:,1].size == 1024 #train_dataset.data[0][:,:,1] has pixels of the second layer of the first image
+assert train_dataset.data[0][:,:,2].size == 1024 #train_dataset.data[0][:,:,2] has pixels of the third layer of the first image
+assert train_dataset.data[1][:,:,0].size == 1024 #train_dataset.data[1][:,:,0] has pixels of the first layer of the second image
+assert train_dataset.data[1][:,:,1].size == 1024 #train_dataset.data[1][:,:,1] has pixels of the second layer of the second image
+assert train_dataset.data[1][:,:,2].size == 1024 #train_dataset.data[1][:,:,2] has pixels of the third layer of the second image
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
