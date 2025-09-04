54fb58952e64cc413284c16d3188df90d6b9cd52
wb4248 <wb4248@rit.edu>
2025-09-04 12:36:42 -0400

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import torchvision
 from torchvision.transforms import ToTensor
-import torch.nn as nn
-import torch.nn.functional as F
 
 train_dataset = torchvision.datasets.CIFAR10(root='data/',train=True,transform=ToTensor())
 test_dataset = torchvision.datasets.CIFAR10(root='data/',train=False,transform=ToTensor())
@@ -16,6 +14,22 @@ assert train_dataset.data[0][:,:,2].size == 1024 #train_dataset.data[0][:,:,2] h
 assert train_dataset.data[1][:,:,0].size == 1024 #train_dataset.data[1][:,:,0] has pixels of the first layer of the second image
 assert train_dataset.data[1][:,:,1].size == 1024 #train_dataset.data[1][:,:,1] has pixels of the second layer of the second image
 assert train_dataset.data[1][:,:,2].size == 1024 #train_dataset.data[1][:,:,2] has pixels of the third layer of the second image
+
+train_dataset_classes = train_dataset.classes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
